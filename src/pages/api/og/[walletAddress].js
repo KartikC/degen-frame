053cb4bf83cee29e753654748266fd4216a72162
kartiksathappan <kartiksathappan@gmail.com>
2024-02-02 13:04:31 -0800

    og better

--- src/pages/api/og/[walletAddress].js
+++ src/pages/api/og/[walletAddress].js
@@ -1,5 +1,7 @@
 // pages/api/og/[walletAddress].js
 import satori from "satori";
+import fs from "fs";
+import path from "path";
 import {
   getTransactionsData,
   getCurrentPrice,
@@ -8,6 +10,11 @@ import {
 
 export default async function handler(req, res) {
   const { walletAddress } = req.query;
+
+  // Load the Inter font file
+  const interFontBuffer = fs.readFileSync(
+    path.resolve(process.cwd(), "src/fonts", "Inter.ttf")
+  );
 
   try {
     const transactionsData = await getTransactionsData(walletAddress);
@@ -36,14 +43,51 @@ export default async function handler(req, res) {
 
     // Generate SVG with Satori
     const svg = await satori(
-      <div style={{ color: "black" }}>
-        <p>{name}</p>
-        <p>${gainLossUsd} USD</p>
-        <p>{gainLossPercentage}%</p>
-      </div>,
+      <svg width="1910" height="1000" xmlns="http://www.w3.org/2000/svg">
+        <img
+          src="https://degen-frame.vercel.app/happy.png"
+          alt=""
+          style={{ position: "absolute", top: 0, left: 0 }}
+        />
+        <div
+          xmlns="http://www.w3.org/1999/xhtml"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            color: "white",
+            fontFamily: "Inter", // Using the Inter font
+            width: "1910px",
+            height: "1000px",
+          }}
+        >
+          <p
+            style={{
+              marginBottom: "20px",
+              fontSize: "48px",
+              color: "#CBD5E1",
+            }}
+          >
+            {name}
+          </p>
+          <p style={{ fontSize: "96px", color: "#38BDF8" }}>${gainLossUsd}</p>
+          <p style={{ fontSize: "72px", color: "#CBD5E1" }}>
+            {gainLossPercentage}%
+          </p>
+        </div>
+      </svg>,
       {
-        width: 800,
-        height: 400,
+        width: 1910,
+        height: 1000,
+        fonts: [
+          {
+            name: "Inter",
+            data: interFontBuffer,
+            weight: 400,
+            style: "normal",
+          },
+        ],
       }
     );
 
