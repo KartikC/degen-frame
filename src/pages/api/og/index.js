7b802929db9851a17a1f55dfe01fcf1ad9471088
kartiksathappan <kartiksathappan@gmail.com>
2024-02-05 15:17:42 -0800

    logz

--- src/pages/api/og/index.js
+++ src/pages/api/og/index.js
@@ -119,6 +119,7 @@ export default async function handler(req, res) {
   if (req.method === "POST") {
     // Handle POST request
     const { untrustedData } = req.body;
+    console.log(untrustedData);
     if (untrustedData && untrustedData.fid) {
       const fid = untrustedData.fid;
       // Make a request to the API
